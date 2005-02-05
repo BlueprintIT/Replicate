33b7810a108a71aab7d3c97c457ca44888f64366
Dave Townsend <dtownsend@oxymoronical.com>
2005-02-05 14:26:26 +0000

    Check in initial code.

---
+++ Replicate/Synchronise.cs
@@ -0,0 +1,51 @@
+/*
+ * $HeadURL$
+ * $LastChangedBy$
+ * $Date$
+ * $Revision$
+ */
+
+using System;
+using System.Windows.Forms;
+using BlueprintIT.Storage;
+
+namespace BlueprintIT.Replicate
+{
+	/// <summary>
+	/// The initial class of a synchronise session.
+	/// </summary>
+	public class Synchronise
+	{
+		private StorageProviders providers = new StorageProviders();
+
+		/// <summary>
+		/// Creates the Synchonise class.
+		/// </summary>
+		public Synchronise()
+		{
+		}
+
+		/// <summary>
+		/// Displays the GUI for a synchronise session.
+		/// </summary>
+		public void ShowGUI()
+		{
+			Application.EnableVisualStyles();
+			//Application.Run(new Gui());
+		}
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args) 
+		{
+			Synchronise sync = new Synchronise();
+			
+			if (args.Length==0)
+			{
+				sync.ShowGUI();
+			}
+		}
+	}
+}
